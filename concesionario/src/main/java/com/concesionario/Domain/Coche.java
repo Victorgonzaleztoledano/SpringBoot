fa80176c248219a092f97346c78bcd0a3468091b
Victorgonzaleztoledano <victor_tala96@hotmail.com>
2023-10-05 19:47:37 +0200

    get, post y put

--- concesionario/src/main/java/com/concesionario/Domain/Coche.java
+++ concesionario/src/main/java/com/concesionario/Domain/Coche.java
@@ -59,27 +59,7 @@ public class Coche {
         if(anyoDeFabricacion < 1990) throw new InvalidArgumentException("Introduzca un año de fabricación válido");
         else this.anyoDeFabricacion = anyoDeFabricacion;
     }
-
-    /*
-        1-Entrada y salida independiente del dominio
-        2-Los endpoints necesitan validar los parámetros
-        3-El endpoint debería de ser capaz de devolver un error
-        4-Añadir servicios
-
-        1-Controller > 2-Servicio(Clases del dominio) > 3-Repositorios(BBDD) > 4-Respuesta
-        DTO: Data Transfer Object. Clases que tienen los datos que necesito para utilizarlos
-        Las excepciones en los constructores no se manipulan solo se lanzan
-
-        @PathVariable son anotaciones de Spring para avisarle de que haga su trabajo
-        @Service para instanciar una vez la clase y recogerla con @Autowired en el controller
-
-        Para sacar la edad habría que restar la fecha actual a la de nacimiento
-        Como normal general los POST no llevan @PathVariable, la variable ya tiene los datos necesarios
-
-        El @PutMapping solo admite el id /coches/{matricula}
-
-        Hay que hacer validaciones en TODOS los constructores de los Input, Output, Update
-        */
+    
     public String getMatricula() {
         return matricula;
     }
